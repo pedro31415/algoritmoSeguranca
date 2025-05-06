9dae0d63f02baf00a4488060ddc56af938536ef9
Pedro <pedrogoku80@gmail.com>
2025-05-06 10:33:18 -0300

    md5

--- md5.py
+++ md5.py
@@ -3,4 +3,13 @@ from Crypto.Hash import MD5
 
 h = MD5.new()
 h.update(b'Jherison safado')
+h1 = MD5.new()
+h1.update(b'Jherison safado')
+print(h.digest())
 print(h.hexdigest())
+
+print(h1.digest())
+print(h1.hexdigest())
+
+print(h == h1)
+print(h.hexdigest() == h1.hexdigest())
