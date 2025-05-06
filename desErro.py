506ca86f468d76a192d7f34beafa9518e125b227
Pedro <pedrogoku80@gmail.com>
2025-05-06 10:32:43 -0300

    gerecao de erro

--- desErro.py
+++ desErro.py
@@ -1,7 +1,6 @@
 
 from Crypto.Cipher import DES
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
 
 def encrypt_des(plaintext, key):
     iv = get_random_bytes(DES.block_size)
@@ -19,17 +18,14 @@ def decrypt_des(ciphertext, key):
 
 if __name__ == "__main__":
     key = get_random_bytes(8)
-    plaintext = b'Jherison safados'
+    plaintext = input('Enter the plaintext: ').encode('utf-8')
 
-    print(f'key: {key}')
-    print()
+    print(f'key: {key} \n')
 
-    print(f'plaintext: {plaintext}')
-    print()
+    print(f'plaintext: {plaintext} \n')
 
     ciphertext = encrypt_des(plaintext, key)
-    print(f'ciphertext: {ciphertext}')
-    print()
+    print(f'ciphertext: {ciphertext} \n')
 
     decrypted_text = decrypt_des(ciphertext, key)
     print(f'decrypted_text: {decrypted_text}')
